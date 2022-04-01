0baed110809cf824da53ad32a45f072f417e7c9b
Nadid Waisque <76746289+NWR57@users.noreply.github.com>
2022-04-01 13:39:01 +0000

    Minor Changes

--- pr_commands/info/budget.js
+++ pr_commands/info/budget.js
@@ -7,10 +7,11 @@ module.exports = {
   description : 'yes',
   run : async(client, message) => {
 
-if(!message.content.startsWith(prefix) || message.author.bot) return;
-const args = message.content.slice(prefix.length).trim().split(' ');
-const command = args.shift().toLowerCase();
-   if(command === 'budget' && args[0] >= 30 && 34 >= args[0]) {
+// if(!message.content.startsWith(prefix) || message.author.bot) return;
+// const args = message.content.slice(prefix.length).trim().split(' ');
+// const command = args.shift().toLowerCase();
+   
+    if(command === 'budget' && args[0] >= 30 && 34 >= args[0]) {
      const row = new Discord.MessageActionRow()
         .addComponents(
           new Discord.MessageSelectMenu()
@@ -35,11 +36,97 @@ const command = args.shift().toLowerCase();
         .setDescription('Yeah fr')
   let burger = await message.reply({ content :' ',ephemeral: true, embed : [pcbuildEmbed], components: [row]}) 
         let intel =new Discord.MessageEmbed()
-          .setTitle('101 Intel xD')
-          .setDescription('COol')
+          .setTitle('Here is the intel build for budget 30-34k')
+          .setDescription('*This build consists of products and parts which may have different price due to the inconsistency of the PC parts market. Please forgive any inconvenience for that cause.*')
+
+          .addFields([
+          {
+            name : 'CPU',
+            value : 'Intel i3 = 10,000tk',
+            inline : true
+          },
+          {
+            name : 'Motherboard',
+            value : ' ',
+            inline : true
+          },
+          {
+            name : 'GPU',
+            value : ' ',
+            inline : true
+          },
+          {
+            name : 'Power Shroud',
+            value : ' ',
+            inline : true
+          },
+          {
+            name : 'RAM',
+            value : 'DDR4 Tridents Z or some shit idk',
+            inline : true
+          },
+          {
+            name : 'Cooler',
+            value : 'Akij fan',
+            inline : true
+          },
+          {
+            name : 'Storage',
+            value : 'SSD',
+            inline : true
+          },
+          {
+            name : 'Storage 2',
+            value : 'M.2 SSD Basically the same thing',
+            inline : true
+          }
+          ])
         let amd =new Discord.MessageEmbed()
-          .setTitle('102 AMD Ryzen xD')
-          .setDescription('COol but not really')
+          .setTitle('Here is the intel build for budget 30-34k')
+          .setDescription('*This build consists of products and parts which may have different price due to the inconsistency of the PC parts market. Please forgive any inconvenience for that cause.*')
+
+          .addFields([
+          {
+            name : 'CPU',
+            value : 'Ryzon Ci Pi You ...amod',
+            inline : true
+          },
+          {
+            name : 'Motherboard',
+            value : ' ',
+            inline : true
+          },
+          {
+            name : 'GPU',
+            value : ' ',
+            inline : true
+          },
+          {
+            name : 'Power Shroud',
+            value : ' ',
+            inline : true
+          },
+          {
+            name : 'RAM',
+            value : 'DDR4 Tridents Z or some shit idk',
+            inline : true
+          },
+          {
+            name : 'Cooler',
+            value : 'Akij fan',
+            inline : true
+          },
+          {
+            name : 'Storage',
+            value : 'SSD',
+            inline : true
+          },
+          {
+            name : 'Storage 2',
+            value : 'M.2 SSD Basically the same thing',
+            inline : true
+          }
+          ])
 
 
 
